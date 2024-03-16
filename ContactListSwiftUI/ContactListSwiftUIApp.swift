88b166a54b948cdfa54c79095983751f13ccd8ab
alexsokolov <frank9154@gmail.com>
2024-03-16 23:45:57 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Александр Соколов on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
